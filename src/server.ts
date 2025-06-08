bf55b5e3667674383dd1c8972c73833cc682e265
IgorRibeiroGuimaraes <igorrguimaraes84@gmail.com>
2025-06-08 17:33:20 -0300

    testing deploy in render

--- src/server.ts
+++ src/server.ts
@@ -72,7 +72,8 @@ app.setErrorHandler((error, req, rep) => {
 });
 
 // Inicia o servidor
-const PORT = process.env.PORT || 3000;
-app.listen({ port: Number(PORT) }).then(() => {
-  console.log(`🚀 Server running `);
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen({ port: PORT, host: '0.0.0.0' }).then(() => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
